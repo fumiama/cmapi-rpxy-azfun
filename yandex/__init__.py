d5fa461e6274fbcd9a677384ed6fe3ac04b792cd
fumiama <s862105088@gmail.com>
2022-03-01 17:21:25 +0800

    add yandex images search proxy

---
+++ yandex/__init__.py
@@ -0,0 +1,20 @@
+import logging
+
+import azure.functions as func
+import cloudscraper
+
+api = "https://yandex.com/images/search"
+
+def getapibody(para: str) -> str:
+    scraper = cloudscraper.create_scraper()
+    u = api + para
+    logging.info("get "+u)
+    s = scraper.get(u)
+    d = s.text
+    s.close()
+    return d
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    para = req.url
+    para = para[para.index("?")+1:]
+    return func.HttpResponse(getapibody(para), status_code=200)
