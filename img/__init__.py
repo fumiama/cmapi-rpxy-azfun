9f6c03d0f75487dfe3c7c920f6072aef13211a6d
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-09-05 00:14:04 +0800

    add: Cached header

--- img/__init__.py
+++ img/__init__.py
@@ -1,4 +1,6 @@
 import logging
+import typing
+import cachetools
 
 import azure.functions as func
 import cloudscraper
@@ -6,23 +8,27 @@ import cloudscraper
 apiprefix = "https://"
 scraper = cloudscraper.create_scraper()
 
-cache = {}
+cache = cachetools.TTLCache(maxsize=1024*1024*1024, ttl=10*60)
 
-def getapibody(para: str) -> bytes:    
+def getapibody(para: str) -> typing.Tuple[bytes, bool]:
     u = apiprefix + para
-    if u in cache.keys():
+    d = cache.get(u)
+    if d is not None:
         logging.info("get cached "+u)
-        return cache[u]
+        return d, True
     logging.info("get new "+u)
     s = scraper.get(u)
     d = s._content
     s.close()
     cache[u] = d
-    return d
+    return d, False
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     para = req.params.get("url")
     if not para or not para.startswith(apiprefix):
         return func.HttpResponse("400 Bad requset", status_code=400)
     para = para[len(apiprefix):]
-    return func.HttpResponse(getapibody(para), status_code=200)
+    d, cached = getapibody(para)
+    resp = func.HttpResponse(d, status_code=200)
+    resp.headers.add_header("Cached", str(cached))
+    return resp
